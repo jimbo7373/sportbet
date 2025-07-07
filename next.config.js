be7c2f485a2c9d037fcca4e3321bb4ca55037c7d
jimbo7373 <grillfeber.se@gmail.com>
2025-07-07 18:15:15 +0200

    Fix: Remove deprecated appDir flag from Next.js config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // Add any necessary configuration here
 }
 
 module.exports = nextConfig 
